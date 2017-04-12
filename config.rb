c017fa8d3fa52b969070b52f6676f34897698aa7
Dan Barber <github@danbarber.me>
2017-04-12 14:45:39 +0100

    CSS based lightbox
    Currently this loads all the full size images on page load. We want to
    load the images when they are views so we should figure out if this is
    possible with just CSS.

--- config.rb
+++ config.rb
@@ -36,6 +36,11 @@ page "/*.txt", layout: false
 # https://middlemanapp.com/basics/helper-methods/
 
 helpers do
+  def parameterize(string)
+    Iconv.conv('ascii//ignore//translit', 'utf-8', string)
+      .gsub(/(\W+)/, "-").downcase
+  end
+
   def srcset(image, sizes)
     sizes.map { |size| srcset_entry(image, size) }.join(", ")
   end
